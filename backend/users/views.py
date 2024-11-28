fe5038b228d13dd3a2de3755c3063c8d22324153
Valentin Rubanov <rubanov.valentin@ya.ru>
2024-11-29 01:23:24 +0300

    foodgram v2.2

--- backend/users/views.py
+++ backend/users/views.py
@@ -3,7 +3,6 @@ from django.contrib.auth import get_user_model
 
 from rest_framework import viewsets, status
 from rest_framework.response import Response
-from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny, IsAuthenticated, IsAdminUser
 from rest_framework.decorators import action
 
